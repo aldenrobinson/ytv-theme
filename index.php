02466e8a8aa21c1f46cec2f9fc256b8ed6a85d6e
Alden Robinson <alden.robinson@gmail.com>
2013-04-01 21:44:29 -0400

    line 6 index.php unexpected :

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<?php //Load the image popups if a gallery page.
 			if( is_post_type_archive('artwork') ) {
 				get_template_part('loop', 'gallery');
-			} else: { //Otherwise get the normal Main content area/sidebar combination.
+			} else { //Otherwise get the normal Main content area/sidebar combination.
 				get_template_part('loop, index');
 				get_sidebar('generic');
 			}
