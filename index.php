088e85f3eb66dc53596508413e161d5ce2dd50d2
Alden Robinson <alden.robinson@gmail.com>
2013-04-01 22:51:52 -0400

    that didn't work

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 	<div class="row">
 		<?php //Load the image popups if a gallery page.
-			if ( is_tax('artwork_type'); ) {
+			if ( is_tax('artwork_type') ) {
 				get_template_part('loop', 'gallery');
 			} else { //Otherwise get the normal Main content area/sidebar combination.
 				get_template_part('loop', 'index');
