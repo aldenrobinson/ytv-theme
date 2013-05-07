c1fbef313185ae0377cc38e7a651370ef00c2146
alden@bigbox <alden.robinson@gmail.com>
2013-05-07 12:11:18 -0400

    got initial highlights working, going to try using the em query and shortcodes instead

--- loop-highlights.php
+++ loop-highlights.php
@@ -1,15 +1,31 @@
 <?php wp_enqueue_script('orbit'); ?>
-<ul id="featured" data-orbit>
+<ul id="highlights" data-orbit>
 	<?php 
-		$args = array( 'post_type' => 'highlight');
-		$highlightquery = new WP_Query ($args);
+
+		$today = date("Y-m-d");
+		$highlightsargs = array( 
+			'post_type' => 'event',
+			'event-categories' => 'highlights',
+			'posts_per_page' => 5
+			//'orderby' => 'meta_value', 
+			//'meta_key' => 'event_start_date',
+			//'order' => 'ASC',
+			//'meta_query' => array(
+				//array(
+					//'key' => 'event_start_date',
+					//'value' => $today,
+					//'compare' => '>=',
+					//'type' => 'DATE'
+				//)
+			//)
+		);
+		$highlightquery = new WP_Query ($highlightsargs);
 		while ( $highlightquery->have_posts() ) : $highlightquery->the_post();
 	?>
 
 		<li data-orbit-slide="highlightslide-<?php the_ID(); ?>">
 			<h5><?php the_title(); ?></h5>
 			<?php the_post_thumbnail('highlightslide'); ?>
-			<?php the_content(); ?>
 		</li>
 
 	<?php endwhile; ?>
