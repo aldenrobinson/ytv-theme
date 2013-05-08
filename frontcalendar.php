25b3a356ce3351e350ddf5003232010313337ed8
Alden Robinson <alden.robinson@gmail.com>
2013-05-08 09:45:59 -0400

    changed to ul li markup in calendar tabs

--- frontcalendar.php
+++ frontcalendar.php
@@ -11,7 +11,7 @@
 	<div class="large-12 columns front-calendar">
 		<?php wp_enqueue_script('section'); ?>
 	 	<div class="section-container auto" data-section>
-			<section id="concerts">
+			<section class="eventtab" id="concerts">
 				<p class="title" data-section-title><a href="#panel1">Concerts</a></p>
 				<div class="content" data-section-content>
 					<?php 
@@ -28,55 +28,61 @@
 					<?php } ?>
 				</div>
 			</section>
-			<section id="festivals">
+			<section class="eventtab" id="festivals">
 				<p class="title" data-section-title><a href="#panel1">Festivals</a></p>
 				<div class="content" data-section-content>
-					<?php 
-						$events = EM_Events::get(array(
-							'orderby' => event_start_date,
-							'category' => 18
-						));
-						foreach ( $events as $event ) {
-					?>
-						<div class="event">
-							<h6><?php echo $event->output('#_EVENTLINK') ?></h6>
-							<a href="<?php the_permalink(); ?>"><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></a>
-						</div>
-					<?php } ?>
+					<ul>
+						<?php 
+							$events = EM_Events::get(array(
+								'orderby' => event_start_date,
+								'category' => 18
+							));
+							foreach ( $events as $event ) {
+						?>
+							<li>
+								<h6><?php echo $event->output('#_EVENTLINK') ?></h6>
+								<a href="<?php the_permalink(); ?>"><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></a>
+							</li>
+						<?php } ?>
+					</ul>
 				</div>
 			</section>
-			<section id="dances">
+			<section class="eventtab" id="dances">
 				<p class="title" data-section-title><a href="#panel2">Dances</a></p>
 				<div class="content" data-section-content>
-					<?php 
-						$events = EM_Events::get(array(
-							'orderby' => event_start_date,
-							'category' => 18
-						));
-						foreach ( $events as $event ) {
-					?>
-						<div class="event">
-							<h6><?php echo $event->output('#_EVENTLINK') ?></h6>
-							<a href="<?php the_permalink(); ?>"><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></a>
-						</div>
-					<?php } ?>
+					<ul>
+						<?php 
+							$events = EM_Events::get(array(
+								'orderby' => event_start_date,
+								'category' => 18
+							));
+							foreach ( $events as $event ) {
+						?>
+							<li>
+								<h6><?php echo $event->output('#_EVENTLINK') ?></h6>
+								<a href="<?php the_permalink(); ?>"><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></a>
+							</li>
+						<?php } ?>
+					</ul>
 				</div>
 			</section>
-			<section id="sessions">
+			<section class="eventtab" id="sessions">
 				<p class="title" data-section-title><a href="#panel3">Sessions &amp; Jams</a></p>
 				<div class="content" data-section-content>
-					<?php 
-						$events = EM_Events::get(array(
-							'orderby' => event_start_date,
-							'category' => 18
-						));
-						foreach ( $events as $event ) {
-					?>
-						<div class="event">
-							<h6><?php echo $event->output('#_EVENTLINK') ?></h6>
-							<a href="<?php the_permalink(); ?>"><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></a>
-						</div>
-					<?php } ?>
+					<ul>
+						<?php 
+							$events = EM_Events::get(array(
+								'orderby' => event_start_date,
+								'category' => 18
+							));
+							foreach ( $events as $event ) {
+						?>
+							<li>
+								<h6><?php echo $event->output('#_EVENTLINK') ?></h6>
+								<a href="<?php the_permalink(); ?>"><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></a>
+							</li>
+						<?php } ?>
+					</ul>
 				</div>
 			</section>
 		</div><!--section container--> 
