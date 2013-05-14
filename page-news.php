dcc03b3f27a7754b33a61cac5feb724703f9df0e
alden@bigbox <alden.robinson@gmail.com>
2013-05-14 15:43:44 -0400

    formatted single events pages

--- page-news.php
+++ page-news.php
@@ -2,10 +2,6 @@
 	<div class="row">
 		<div class="large-12 columns" id="content">
 			<div class="row">
-				<?php 
-					get_template_part('loop', 'index');
-					get_sidebar('generic');
-				?>
 			</div>
 		</div>
 	</div>
