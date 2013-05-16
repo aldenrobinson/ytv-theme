ba4ab8588b17fcbd6575fec2f66583d3cd0dcb50
alden@bigbox <alden.robinson@gmail.com>
2013-05-16 17:17:46 -0400

    got blurb and submit box onto events page. need to center button on submit box

--- footer.php
+++ footer.php
@@ -21,7 +21,13 @@
 		?>
 		<a class="close-reveal-modal">&#215;</a>
 	</div>
-
+	<?php if ( is_single('events') ) { ?>
+		<div id="event-submit-popup" class="reveal-modal">
+			<h5>Submit an event</h5>
+				<?php echo do_shortcode('[contact-form-7 id="321" title="Submit an Event"]'); ?>
+			<a class="close-reveal-modal">&#215;</a>
+		</div>
+	<?php } ?>
 <?php wp_footer(); ?>
 </body>
 </html>
--- page-events.php
+++ page-events.php
@@ -2,6 +2,14 @@
 	<div class="row">
 		<div class="large-12 columns" id="content">
 			<div class="row">
+				<?php get_template_part('loop', 'index'); ?>
+				<aside id="event-submit" class="large-4 columns">
+					<h4>Submit an event</h4>
+					<p>Got an event to list here? Give us the details.</p>
+					<a class="button" data-reveal-id="#event-submit-popup" href="#">Submit your event</a>
+				</aside>
+			</div>
+			<div class="row">
 				<div class="large-12.columns events-tabset">
 					<?php 
 						get_template_part('loop', 'events');
