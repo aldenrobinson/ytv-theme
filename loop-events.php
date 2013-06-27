a36a56828579240e7cf8eec02fcc7967a1564086
Alden Robinson <alden.robinson@gmail.com>
2013-06-27 08:12:45 -0400

    added more feeds to events loop

--- loop-events.php
+++ loop-events.php
@@ -2,7 +2,7 @@
 <div class="section-container auto" data-section data-options="deep_linking: true">
 	<section class="eventtab">
 		<p class="title" data-section-title><a href="#concerts">Concerts</a></p>
-		<div class="content equal_height" data-section-content data-slug="concerts">
+		<div class="content " data-section-content data-slug="concerts">
 			<ul>
 				<?php 
 					$events = EM_Events::get(array(
@@ -17,12 +17,11 @@
 					</li>
 				<?php } ?>
 			</ul>
-			<a class="jump" href="#">See all &raquo;</a>
 		</div>
 	</section>
 	<section class="eventtab" id="festivals">
 		<p class="title" data-section-title><a href="#festivals">Festivals</a></p>
-		<div class="content equal_height" data-section-content data-slug="festivals">
+		<div class="content " data-section-content data-slug="festivals">
 			<ul>
 				<?php 
 					$events = EM_Events::get(array(
@@ -41,7 +40,7 @@
 	</section>
 	<section class="eventtab"> 
 		<p class="title" data-section-title><a href="#dances">Dances</a></p>
-		<div class="content equal_height" data-section-content data-slug="dances">
+		<div class="content " data-section-content data-slug="dances">
 			<ul>
 				<?php 
 					$events = EM_Events::get(array(
@@ -60,12 +59,145 @@
 	</section>
 	<section class="eventtab">
 		<p class="title" data-section-title><a href="#sessions">Sessions &amp; Jams</a></p>
-		<div class="content equal_height" data-section-content data-slug="sessions">
+		<div class="content " data-section-content data-slug="sessions">
 			<ul>
 				<?php 
 					$events = EM_Events::get(array(
 						'orderby' => event_start_date,
 						'category' => 30
+					));
+					foreach ( $events as $event ) {
+				?>
+					<li>
+						<h5><?php echo $event->output('#_EVENTLINK') ?></h5>
+						<p><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></p>
+					</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</section>
+	<section class="eventtab">
+		<p class="title" data-section-title><a href="#sessions">Radio &amp; TV</a></p>
+		<div class="content " data-section-content data-slug="sessions">
+			<ul>
+				<?php 
+					$events = EM_Events::get(array(
+						'orderby' => event_start_date,
+						'category' => 24
+					));
+					foreach ( $events as $event ) {
+				?>
+					<li>
+						<h5><?php echo $event->output('#_EVENTLINK') ?></h5>
+						<p><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></p>
+					</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</section>
+	<section class="eventtab">
+		<p class="title" data-section-title><a href="#sessions">Workshops</a></p>
+		<div class="content " data-section-content data-slug="sessions">
+			<ul>
+				<?php 
+					$events = EM_Events::get(array(
+						'orderby' => event_start_date,
+						'category' => 26
+					));
+					foreach ( $events as $event ) {
+				?>
+					<li>
+						<h5><?php echo $event->output('#_EVENTLINK') ?></h5>
+						<p><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></p>
+					</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</section>
+	<section class="eventtab">
+		<p class="title" data-section-title><a href="#sessions">Singing</a></p>
+		<div class="content " data-section-content data-slug="sessions">
+			<ul>
+				<?php 
+					$events = EM_Events::get(array(
+						'orderby' => event_start_date,
+						'category' => 61
+					));
+					foreach ( $events as $event ) {
+				?>
+					<li>
+						<h5><?php echo $event->output('#_EVENTLINK') ?></h5>
+						<p><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></p>
+					</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</section>
+	<section class="eventtab">
+		<p class="title" data-section-title><a href="#sessions">Classes</a></p>
+		<div class="content " data-section-content data-slug="sessions">
+			<ul>
+				<?php 
+					$events = EM_Events::get(array(
+						'orderby' => event_start_date,
+						'category' => 14
+					));
+					foreach ( $events as $event ) {
+				?>
+					<li>
+						<h5><?php echo $event->output('#_EVENTLINK') ?></h5>
+						<p><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></p>
+					</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</section>
+	<section class="eventtab">
+		<p class="title" data-section-title><a href="#sessions">Camps</a></p>
+		<div class="content " data-section-content data-slug="sessions">
+			<ul>
+				<?php 
+					$events = EM_Events::get(array(
+						'orderby' => event_start_date,
+						'category' => 27
+					));
+					foreach ( $events as $event ) {
+				?>
+					<li>
+						<h5><?php echo $event->output('#_EVENTLINK') ?></h5>
+						<p><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></p>
+					</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</section>
+	<section class="eventtab">
+		<p class="title" data-section-title><a href="#sessions">Exhibits</a></p>
+		<div class="content " data-section-content data-slug="sessions">
+			<ul>
+				<?php 
+					$events = EM_Events::get(array(
+						'orderby' => event_start_date,
+						'category' => 21
+					));
+					foreach ( $events as $event ) {
+				?>
+					<li>
+						<h5><?php echo $event->output('#_EVENTLINK') ?></h5>
+						<p><?php echo $event->output('#_EVENTDATES') ?> @ <?php echo $event->output('#_LOCATIONNAME') ?></p>
+					</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</section>
+	<section class="eventtab">
+		<p class="title" data-section-title><a href="#sessions">For families</a></p>
+		<div class="content " data-section-content data-slug="sessions">
+			<ul>
+				<?php 
+					$events = EM_Events::get(array(
+						'orderby' => event_start_date,
+						'category' => 29
 					));
 					foreach ( $events as $event ) {
 				?>
